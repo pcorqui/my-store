d2b0a38abf4bf0f7efc6c33010a55a9df641845c
Paul Cortes <pcorqui@gmail.com>
2024-03-25 22:48:26 -0600

    UPDATE: property_binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   name = "paul"
   edad = 18;
   imagen = "https://source.unsplash.com/random";
+  btnDisabled = false;
+  img = "https://source.unsplash.com/random"
+
+  persona = {
+    name: "paul",
+    age: 18,
+    img: "https://source.unsplash.com/random",
+  }
 }
