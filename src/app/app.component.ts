22c341ffda47f3f407e8383048b58450a5ce0523
Paul Cortes <pcorqui@gmail.com>
2024-03-25 22:38:02 -0600

    UPDATE: string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = "paul"
+  edad = 18;
+  imagen = "https://source.unsplash.com/random";
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,7 @@
+const username: string | number = 'nicobytes';
+
+const sum = (a: number, b: number) =>{
+    return a + b;
+}
+
+sum(1,3);
